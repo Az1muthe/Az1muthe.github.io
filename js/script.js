dbdcdf74978d8761b190a6014fef82fa35aab3f2
Parenthou Adrien <97531184+Az1muthe@users.noreply.github.com>
2023-01-03 11:23:53 +0100

    Bug fixes
    Fixed some bugs and incorrect links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+}
+
+
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
